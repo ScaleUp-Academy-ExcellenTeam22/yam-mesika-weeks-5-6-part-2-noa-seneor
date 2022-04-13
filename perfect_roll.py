e9d62f26dd5be6a2c48edf46e9f7232a62aa1e18
noa-seneor <100723117+noa-seneor@users.noreply.github.com>
2022-04-13 21:03:01 +0200

    Update perfect_roll.py

--- perfect_roll.py
+++ perfect_roll.py
@@ -1,13 +1,15 @@
 def perfect_roll():
     """
-    function that generates perfect numbers infinitely
-    :return: generator of perfect number
+    Function that generates perfect numbers infinitely
+    Perfect number : a positive integer that is equal to the sum of its proper divisors
+    :return: generator of perfect numbers
     """
-    i = 6
+    # starting from 6 because smallest perfect number is 6
+    perfect_num = 6
     while True:
-        if i == sum([x for x in range(1, i//2 + 1) if i % x == 0]):
-            yield i
-        i += 1
+        if perfect_num == sum([x for x in range(1, perfect_num//2 + 1) if perfect_num % x == 0]):
+            yield perfect_num
+        perfect_num += 1
 
 
 def main():
